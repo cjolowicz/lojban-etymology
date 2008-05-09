53e98e2d7320c17dce1abc470856d076832cf42f
Mublin <mublin@dealloc.org>
2008-05-09 11:28:07 +0000

    Imported from ../lojban-source-words_es-1.6.tar.gz.

--- es/htmlize.py
+++ es/htmlize.py
@@ -43,7 +43,7 @@ dt { margin-top: 1em; }
 </head>
 
 <body>
-<h1>Spanish etymology of Lojban<br/><span id="version">Version 1.5, April 2008</span></h1>
+<h1>Spanish etymology of Lojban<br/><span id="version">Version 1.6, April 2008</span></h1>
 
 <hr/>
 <div class="index">
